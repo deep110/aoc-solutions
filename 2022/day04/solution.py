07a11a8019c0493f94a549159575e0c9e0375206
Deepankar Agrawal <deepankar11093@gmail.com>
2022-12-07 13:07:25 +0530

    add day 04

--- 2022/day04/solution.py
+++ 2022/day04/solution.py
@@ -1,13 +1,37 @@
 from os import path
+import re
 
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
     ms = f.readlines()
 
+PATTERN = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
+
+tasks = []
+for i in ms:
+    p = PATTERN.search(i)
+    tasks.append([(int(p[1]), int(p[2])), (int(p[3]), int(p[4]))])
+
 def part1():
-    pass
+    total = 0
+
+    for e1, e2 in tasks:
+        if (e2[0] >= e1[0] and e2[1] <= e1[1]) or (e1[0] >= e2[0] and e1[1] <= e2[1]):
+            total += 1
+
+    return total
 
 def part2():
-    pass
+    total = 0
+
+    for e1, e2 in tasks:
+        if (e1[0] > e2[0]):
+            if e1[0] >= e2[0] and e1[0] <= e2[1]:
+                total += 1
+        else:
+            if e2[0] >= e1[0] and e2[0] <= e1[1]:
+                total += 1
+
+    return total
 
 print("Part1 solution: ", part1())
 print("Part2 solution: ", part2())
